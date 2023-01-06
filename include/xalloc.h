822d934176ed7c34e8fb0dd431ce740b151edba1
clara.chalumeau <clara.chalumeau@epita.fr>
2023-01-06 20:59:36 +0100

    Xalloc implementation

--- include/xalloc.h
+++ include/xalloc.h
@@ -5,19 +5,45 @@
 #include <stddef.h>
 #include <stdlib.h>
 
+extern xalloc *xalloc_dlist;
+/**
+ * @brief Structure of the memory allocation
+ * @param data the data pointer
+ * @param next pointer to the next block of memory
+ * @param prev pointer to the previous block of memory
+ */
 struct xalloc_item
 {
-    void *ptr;
+    void *data;
     struct xalloc_item *next;
     struct xalloc_item *prev;
 };
 
+/**
+ * @brief The doubled linked list
+ * @param size The size of the double linked list
+ * @param head The first block of the linked list
+ * @param tail The last block of the linked list
+ */
 struct xalloc
 {
     size_t size;
     struct xalloc_item *head;
     struct xalloc_item *tail;
 };
+
+/**
+ * @return the initialize dlist of pointer
+ */
+struct xalloc *xalloc_init(void);
+
+/**
+ * @brief change the location of the pointer
+ * @param data the pointer to convert
+ * @param size the distance
+ * @return the pointer to the new location
+ */
+void *to_data(void *data, size_t size);
 
 /**
  * @brief Malloc wrapper that exits the program if the allocation fails
--- src/utils/xalloc.c
+++ src/utils/xalloc.c
@@ -1,15 +1,33 @@
 #include "xalloc.h"
 
+struct xalloc *xalloc_init(void)
+{
+    struct xalloc *xalloc = malloc(sizeof (struct xalloc));
+    if (xalloc == NULL)
+        err(EXIT_FAILURE, "xalloc_init: Failed to init.");
+    xalloc->head = NULL;
+    xalloc->tail = NULL;
+    xalloc->size = NULL;
+    xalloc_dlist = xalloc;
+}
+
+void *to_data(void *data, size_t size)
+{
+    char *input = data;
+    return (input + move);
+}
+
 void *xmalloc(size_t nmemb, size_t size)
 {
     size_t total_size = 0;
     if (__builtin_umull_overflow(nmemb, size, &total_size))
         errx(1, "xmalloc: overflow detected : %zu * %zu", nmemb, size);
-    void *res = malloc(total_size);
-    if (!res)
+    struct xalloc_item *temp = NULL;
+    temp = malloc(total_size);
+    if (!temp)
         err(EXIT_FAILURE, "xmalloc: malloc failed, size: %zu", total_size);
-
-    return res;
+    push_back(temp);
+    return temp;
 }
 
 void *xcalloc(size_t nmemb, size_t size)
@@ -20,12 +38,13 @@ void *xcalloc(size_t nmemb, size_t size)
         errx(EXIT_FAILURE, "xmalloc: overflow detected : %zu * %zu", nmemb,
              size);
 #endif
-    void *res = calloc(nmemb, size);
-    if (!res)
+    struct xalloc_item *temp = NULL;
+    temp = calloc(nmemb, size);
+    if (!temp)
         err(EXIT_FAILURE, "xcalloc: calloc failed, nmemb: %zu, size: %zu",
             nmemb, size);
-
-    return res;
+    push_back(temp);
+    return temp;
 }
 
 void *xrealloc(void *ptr, size_t nmemb, size_t size)
@@ -33,14 +52,70 @@ void *xrealloc(void *ptr, size_t nmemb, size_t size)
     size_t total_size = 0;
     if (__builtin_umull_overflow(nmemb, size, &total_size))
         errx(1, "xrealoc: overflow detected : %zu * %zu", nmemb, size);
-    void *res = realloc(ptr, nmemb * size);
-    if (!res)
+    struct xalloc_item *temp = NULL;
+    void *temp = realloc(ptr, nmemb * size);
+    if (!temp)
         err(EXIT_FAILURE, "xrealloc: realloc failed, size: %zu", total_size);
+    push_back(temp);
+    return temp;
+}
 
-    return res;
+void push_back(struct xalloc_item temp)
+{
+    void *data = to_data(temp, sizeof(xalloc_item));
+    temp->data = data;
+    temp->next = NULL;
+
+    if (xalloc_dlist->tail == NULL)
+    {
+        temp->prev = NULL;
+        xalloc_dlist->head = xalloc_dlist->tail = temp;
+    }
+    else
+    {
+        xalloc_dlist->tail->next = temp;
+        temp->prev = xalloc_dlist->tail;
+        list->tail = temp;
+    }
+    xalloc_dlist->size++;
+}
+
+void xfree(void *ptr)
+{
+    if (ptr == NULL || xalloc_dlist == NULL)
+        err(EXIT_FAILURE, "xfree: nothing to free");
+
+    xalloc *temp = to_data(ptr, -sizeof(xalloc_item));
+
+    // Head
+    if (temp->prev == NULL)
+        xalloc_dlist->head = temp->next;
+    else
+        temp->prev->next = temp->next;
+
+    // Tail
+    if (temp->next == NULL)
+        xalloc_dlist->tail = temp->prev;
+    else
+        temp->prev->next = temp->prev;
+
+    free(temp);
+
 }
 
 void xfree_all(void)
 {
-    // TODO: implement
+    if (xalloc_dlist != NULL)
+    {
+        struct xalloc_item *temp = xalloc_dlist->head;
+        while (temp != NULL)
+        {
+            struct xalloc_item *tmp = temp;
+            temp = temp->next;
+            free(tmp);
+        }
+        xalloc_dlist->head = NULL;
+        xalloc_dlist->tail = NULL;
+        free(xalloc_dlist);
+    }
 }
