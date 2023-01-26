ae9a35450d0ba817258ea1912f07a0f384edc3c3
Clara Chalumeau <chalumeauclara@gmail.com>
2023-01-26 21:01:33 +0100

    FIX multiple newline at the beginning

--- src/parser/parser_input.c
+++ src/parser/parser_input.c
@@ -14,30 +14,20 @@ struct ast *parser_input(struct token *list_tokens)
     struct ast *ast = NULL;
     struct token_list *tokens = xmalloc(1, sizeof(struct tokens*));
     tokens->current_token = list_tokens;
-    switch (tokens->current_token->type)
-    {
-        case NEWLINE :
-            eat(tokens, NEWLINE);
-            break;
-        case EOF :
-            eat(tokens, EOF);
-            break;
-        default:
-            ast = parser_list(tokens);
-            //printf("input %i\n", tokens->current_token->type);
-            if (tokens->current_token == NULL)
-                return ast;
-            switch (tokens->current_token->type) {
-                case NEWLINE :
-                    eat(tokens, NEWLINE);
-                    break;
-                case EOF :
-                    eat(tokens, EOF);
-                    break;
-                default:
-                    wrong_look_ahead(tokens, "parse_input");
-            }
-    }
+
+    while (look_ahead(tokens) == NEWLINE)
+        eat(tokens, NEWLINE);
+
+    ast = parser_list(tokens);
+    //printf("input %i\n", tokens->current_token->type);
+    if (tokens->current_token == NULL)
+        return ast;
+
+    while (look_ahead(tokens) == NEWLINE)
+        eat(tokens, NEWLINE);
+
+    if (tokens->current_token != NULL)
+        wrong_look_ahead(tokens, "parse_input");
 
     return ast;
 }
